ed639fd25d7c4493cdeacebe96809068fbefdcd6
Sarah <shaswell@scottlogic.com>
2025-12-04 17:37:14 +0000

    Fix day 1 part 1, issue was missing last char of last line off

--- day1/solve.py
+++ day1/solve.py
@@ -14,18 +14,13 @@ def applyNewInstruction(prev, direction, delta):
     # print(newPosition%100)
     return newPosition%100
 
-# position here is always a positive number
+# position here is always a positive number or zero
 def getPasses(position, direction, delta):
     numPasses = 0
 
     # print("position " + str(position))
     # print("direction " + str(direction))
     # print("delta " + str(delta))
-
-    # if(delta >= 100):
-    #     numPasses = math.floor(delta/100)
-
-# add conditions for hitting 100/0
 
     # not a new hit
     if(delta == 0):
@@ -34,20 +29,17 @@ def getPasses(position, direction, delta):
     # number of full rotations
     numPasses = math.floor(delta/100)
 
+    if(position%100 == 0):
+        return numPasses
+
     # partial rotations
     simplifiedDelta = delta%100
 
-    
-
-    # # hitting zero
-    # if(applyNewInstruction(position, direction, delta) == 0):
-    #     return numPasses
-
     if(direction == 'L'):
-        if(delta%100 >= position): # then we're going to or past zero
+        if(simplifiedDelta >= position): # then we're going to or past zero
             return numPasses + 1
     else:
-        if(position + delta%100 >= 100): # then we're going to or past zero
+        if(position + simplifiedDelta >= 100): # then we're going to or past zero
             return numPasses + 1
     
     # print("passes " + str(numPasses))
@@ -60,16 +52,24 @@ def part1(lines):
 
     # for each line, interpret input
     for line in lines:
+        print(line)
+
         # split line by removing first entry
         direction = line[0]
-        delta = int(line[1:-1])
+        delta = int(line[1:])
 
         # do the needful
         newPosition = applyNewInstruction(position, direction, delta)
         if (newPosition == 0):
             zeroHits = zeroHits + 1
 
+        # print("Hits: " + str(zeroHits))
+
         passes = getPasses(position, direction, delta)
+        
+        # print("Go " + direction + " by " + str(delta) + " from " + str(position))
+        # print("-> passes: " + str(passes))
+
         zeroPasses = zeroPasses + passes
 
         position = newPosition
@@ -78,13 +78,13 @@ def part1(lines):
     # (repeat)
 
     print("Hit zero " + str(zeroHits) + " times") # answer 3 or 1018
-    print("Passed zero " + str(zeroPasses) + " times") # answer 6 or ...
+    print("Passed zero " + str(zeroPasses) + " times") # answer 6 or 5815
 
-    print("Combined: " + str(zeroHits + zeroPasses))
+    # print("Combined: " + str(zeroHits + zeroPasses))
 
 print("Solve day 1")
 
-lines: list[str] = utils.readFileToLines('./day1/sample')
+lines: list[str] = utils.readFileToLines('./day1/input')
 
 part1(lines)
 
