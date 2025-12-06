f1c61f98c26c544702e53d567dbe4d88a321bb30
Sarah <48217891+sehaswell@users.noreply.github.com>
2025-12-06 11:17:05 +0000

    Add stripped line file read code from 2023

--- utils/fileUtils.py
+++ utils/fileUtils.py
@@ -4,11 +4,15 @@ def readFile(path):
     file.close()
     return content
 
-def readFileToLines(path):
+def readFileToLines(path, strip = False):
     file = open(path)
     content = file.readlines()
     file.close()
-    return content
+
+    if (not strip):
+        return content
+
+    return [line.strip() for line in content]
 
 def stripLines(lines):
     return [line.strip() for line in lines]
